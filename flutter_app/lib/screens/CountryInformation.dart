8d776a2eacbb36b817bc8736c46717f10f78099f
crisven <crisven1998@gmail.com>
2020-04-16 16:56:33 -0500

    Merge branch 'master' of https://github.com/BryanManzano2016/BestPlaces
     Conflicts:
    	flutter_app/lib/screens/ComparePlacesScreen.dart
    	flutter_app/lib/screens/ScreenRankingPlaces.dart

--- flutter_app/lib/screens/CountryInformation.dart
+++ flutter_app/lib/screens/CountryInformation.dart
@@ -1,67 +1,138 @@
 import 'dart:ui';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class CountryInformation extends StatelessWidget {
+class ScreenRankingPlaces extends StatelessWidget {
     final String placesby;
-    CountryInformation({Key key, this.placesby}) : super(key: key);
+    ScreenRankingPlaces({Key key, this.placesby}) : super(key: key);
     @override
     Widget build(BuildContext context) {
         return SafeArea(child:Scaffold(
-            body: countryInfo(),
+            body:  countryInfo(),
         ));
     }
 }
 
-
-
 class countryInfo extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
+    @override
+    Widget build(BuildContext context) {
 
-      final coutryImg = Padding(
-        padding: EdgeInsets.all(8.0),
-          child: CircleAvatar(
-              radius: 72.0,
-              backgroundColor: Colors.transparent,
-              backgroundImage: AssetImage('assets/images/Ecuador.png'),
-          ),
-      );
-
-
-      final countryName = Padding(
-        padding: EdgeInsets.all(8.0),
-          child: Text(
-              'ECUADOR',
-                  style: TextStyle(fontSize: 28.0, color: Colors.blue),
-          ),
-      );
+        final countryImg = Container(
+            width: 190,
+            height: 190,
+            padding: const EdgeInsets.all(8.0),
+            decoration: new BoxDecoration(
+                //shape: BoxShape.circle,
+                image: new DecorationImage(
+                    fit: BoxFit.fill,
+                    image: ExactAssetImage('assets/images/Ecuador.png'),
+                )
+            ),
+        );
 
 
-      final description = Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Text(
-              'Ecuador es un país que se extiende por el ecuador en la costa oeste de Sudamérica. Sus diversos paisajes abarcan la selva del Amazonas, las zonas altas andinas y las islas Galápagos de abundante fauna.',
-              style: TextStyle(fontSize: 28.0, color: Colors.blue),
-          ),
-      );
+        final countryName = Container(
+            padding: EdgeInsets.only(top: 15, bottom: 10),
+            child: Text(
+                'ECUADOR',
+                style: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),
+            ),
+        );
 
 
-    return ListView(
-        padding: const EdgeInsets.all(10.0),
-        children: <Widget>[
-            Column(
+        final description = Container(
+            padding: EdgeInsets.only(top: 10, bottom: 10),
+            child: Text(
+                'Ecuador es un país que se extiende por el ecuador en la costa oeste de Sudamérica. Sus diversos paisajes abarcan '
+                    'la selva del Amazonas, las zonas altas andinas y las islas Galápagos de abundante fauna.',
+                style: TextStyle(fontSize: 18.0, ),
+            ),
+        );
+
+
+        final countryData = Container(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                    coutryImg,
-                    countryName,
-                    description,
+                    Container(
+                        padding: EdgeInsets.only(top: 10, bottom: 10),
+                        child: Text(
+                            'Continente: America',
+                            style: TextStyle(fontSize: 18.0 ),
+                        ),
+                    ),
+
+                    Container(
+                        padding: EdgeInsets.only(top: 10, bottom: 10),
+                        child: Text(
+                            'Poblacion: 100000',
+                            style: TextStyle(fontSize: 18.0 ),
+                        ),
+                    ),
+
+                    Container(
+                        padding: EdgeInsets.only(top: 10, bottom: 10),
+                        child: Text(
+                            'Clima: 30 grados',
+                            style: TextStyle(fontSize: 18.0 ),
+                        ),
+                    ),
+
                 ],
             ),
-        ],
-    );
-  }
+        );
+
+
+        Column _buildButtonColumn(IconData icon, String label) {
+            return Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                    Icon(icon, color: Colors.green[500], size: 30,),
+                    Container(
+                        child: Text(
+                            label,
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w800,
+                                fontFamily: 'Roboto',
+                                letterSpacing: 0.5,
+                                fontSize: 18,
+                                height: 2,
+                            ),
+                        ),
+                    ),
+                ],
+            );
+        }
+
+
+        final buttonSection = Container(
+
+            padding: EdgeInsets.only(top: 40),
+
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                    _buildButtonColumn(Icons.star, '7/10'),
+                    _buildButtonColumn(Icons.star_border, 'Rate'),
+                    _buildButtonColumn(Icons.comment, 'Reviews'),
+                ],
+            ),
+        );
+
+
+
+        return ListView(
+            padding: EdgeInsets.all(10),
+            children: <Widget>[
+                countryImg,
+                countryName,
+                description,
+                countryData,
+                buttonSection,
+            ],
+        );
+    }
+
 }
-
-
-
-
-
